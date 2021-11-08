b0406cc7640431c3880edb78bae3fdd474926044
Isaac Yep <sleepyboy@Isaacs-MacBook-Air.local>
2021-11-07 21:04:19 -0800

    updated and orginized program networking application entry point.

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+//-------------------------------------------
+// index.js
+// 		-- Isaac Yep; CS-594 Project
+//-------------------------------------------
 
-// Create a new client instance
+// Dependencies
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
+
+//---Commands handler------------------------
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+
+//---Events handlers-------------------------
+// ready
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Login to Discord with your client's token
+// interactionCreate
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+//---Login-----------------------------------
 client.login(token);
