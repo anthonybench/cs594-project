73944826848ee7832030a44015f697e8e453e5b9
Isaac Yep <sleepyboy@Isaacs-MacBook-Air.local>
2021-11-10 21:51:41 -0800

    updated to conceal bot token in python functions.

--- python-version/list-channels.py
+++ python-version/list-channels.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
 import discord
+from json import load;
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+with open("data.json", "r") as json_file:
+  secrets = json.load(json_file)
+  class MyClient(discord.Client):
+      async def on_ready(self):
+          print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        ress = client.get_all_channels()
+      async def on_message(self, message):
+          print('Message from {0.author}: {0.content}'.format(message))
+          ress = client.get_all_channels()
 
-        res = []
-        for i in ress:
-          res.append(i.name)
-        print(res)
+          res = []
+          for i in ress:
+            res.append(i.name)
+          print(res)
 
-client = MyClient()
-client.run('OTAyNzgwOTczOTcwNzYzNzc2.YXjaTQ.f8iLp0A1nbrlJFtkHhsegQ5jlTo')
+  client = MyClient()
+  client.run(secrets.token)
