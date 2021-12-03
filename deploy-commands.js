ad35f895b04fa9f931f8151b3384b5218419e635
Isaac Yep <sleepyboy@Isaacs-MacBook-Air.local>
2021-12-02 19:27:54 -0800

    slight pedantic change to code style in js feature execution entry.

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,26 +3,24 @@
 // 		-- Isaac Yep; CS-594 Project
 //-------------------------------------------
 
-// Dependencies
+
+
+//---Dependencies----------------------------
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
-
 //---Commands handler------------------------
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-
 //---Register--------------------------------
 const rest = new REST({ version: '9' }).setToken(token);
-
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
